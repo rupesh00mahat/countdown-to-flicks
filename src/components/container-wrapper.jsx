d2ef7277b47092bb25f60b8d64e856aa00e564db
rupesh00mahat <rupesh00mahat@gmail.com>
2024-06-27 21:30:22 +0545

    add days to count

--- src/components/container-wrapper.jsx
+++ src/components/container-wrapper.jsx
@@ -1,40 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import MovieContainer from './movie-container'
+import React, { useEffect, useState } from "react";
+import MovieContainer from "./movie-container";
 
 function ContainerWrapper() {
+  const [movies, setMovies] = useState([]);
 
-    const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTRjYTUxZmY3MjljN2Q5YzA0OTA4ZmM3OTIzODg2MyIsIm5iZiI6MTcxOTE1NDg3MC41MTE5MjUsInN1YiI6IjY2NzViMTM2YjFjN2I1OGZlMjIxYzBkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bCoEIJ2F2XiYmE0lczivfWhlm33S15vhEcnNnUMLInM",
+      },
+    };
 
-    useEffect(()=>{
-        const options = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTRjYTUxZmY3MjljN2Q5YzA0OTA4ZmM3OTIzODg2MyIsIm5iZiI6MTcxOTE1NDg3MC41MTE5MjUsInN1YiI6IjY2NzViMTM2YjFjN2I1OGZlMjIxYzBkOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bCoEIJ2F2XiYmE0lczivfWhlm33S15vhEcnNnUMLInM'
-            }
-          };
-          
-          fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', options)
-            .then(response => response.json())
-            .then(response => {
-              console.log(response)
-              setMovies(response.results)})
-            .catch(err => console.error(err));
-    },[])
+    fetch(
+      "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setMovies(response.results);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
-    <div className='movies-list'>
-           {movies.map((data)=>{
-return <MovieContainer
-key={data.id}
-title={data.title}
-releaseDate={data.release_date}
-poster={data.poster_path}
-description={data.overview}
-/>
-           })}
+    <div className="movies-list">
+      {movies.map(({id, title,release_date, poster_path, overview}) => {
+        if(new Date(release_date.toString()) > new Date()){
+          return (
+            <MovieContainer
+              key={id}
+              title={title}
+              releaseDate={release_date}
+              poster={poster_path}
+              description={overview}
+            />
+          );
+        }
+      })}
     </div>
-  )
+  );
 }
 
-export default ContainerWrapper
+export default ContainerWrapper;
--- src/components/movie-container.jsx
+++ src/components/movie-container.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 function MovieContainer({title, releaseDate, poster, description}) {
+  const [daysLeft, setDaysLeft] = useState(0);
+
+  useEffect(()=>{
+    setDaysLeft(Math.ceil((new Date(releaseDate.toString()) - new Date())/(1000 * 60 * 60 * 24)))
+  },[releaseDate])
   return (
     <div className='movie-container'>
-
+      <div className='days-until-wrapper'>
+{daysLeft} Days to go
+      </div>
         <img src={`http://image.tmdb.org/t/p/w500${poster}`}/>
         <div className="container-content">
             <h3>
