2f4e9387281736e2383712ea6a0e18d01762d916
rupesh00mahat <rupesh00mahat@gmail.com>
2024-06-28 20:15:18 +0545

    redesign movie-list

--- src/components/movie-container.jsx
+++ src/components/movie-container.jsx
@@ -1,28 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-function MovieContainer({title, releaseDate, poster, description}) {
+function MovieContainer({ title, releaseDate, poster, description }) {
   const [daysLeft, setDaysLeft] = useState(0);
 
-  useEffect(()=>{
-    setDaysLeft(Math.ceil((new Date(releaseDate.toString()) - new Date())/(1000 * 60 * 60 * 24)))
-  },[releaseDate])
+  useEffect(() => {
+    setDaysLeft(
+      Math.ceil(
+        (new Date(releaseDate.toString()) - new Date()) / (1000 * 60 * 60 * 24)
+      )
+    );
+  }, [releaseDate]);
   return (
-    <div className='movie-container'>
-      <div className='days-until-wrapper'>
-{daysLeft} Days to go
+    <div className="movie-container">
+      <div className="days-until-wrapper">{daysLeft} Days to go</div>
+      <img src={`http://image.tmdb.org/t/p/w500${poster}`} />
+      <div className="container-content">
+        <h3>{title}</h3>
+        <p>{description}</p>
       </div>
-        <img src={`http://image.tmdb.org/t/p/w500${poster}`}/>
-        <div className="container-content">
-            <h3>
-                {title}
-            </h3>
-            <p>{description}</p>
-        </div>
-        <div className="countdown-wrapper">
-            {releaseDate}
-        </div>
+      <div className="countdown-wrapper">{releaseDate}</div>
     </div>
-  )
+  );
 }
 
-export default MovieContainer
+export default MovieContainer;
