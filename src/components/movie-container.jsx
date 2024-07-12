b22fb6a6ee35ec9446ae33ce1e6adca8d70f5697
rupesh00mahat <rupesh00mahat@gmail.com>
2024-07-12 20:52:47 +0545

    change movie card css

--- src/components/movie-container.jsx
+++ src/components/movie-container.jsx
@@ -9,10 +9,9 @@ function MovieContainer({ title, releaseDate, poster, description }) {
     <div className="movie-container">
       <img src={`http://image.tmdb.org/t/p/w500${poster}`} />
       <div className="container-content">
-        <h3>{title}</h3>
+        <h3 className="semi-title">{title}</h3>
         <p>{description}</p>
       </div>
-      <div className="countdown-wrapper">{releaseDate}</div>
     </div>
   );
 }
