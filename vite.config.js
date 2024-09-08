c5117ee9f96263f840800871b81d32a86ded84c2
rupesh00mahat <rupesh00mahat@gmail.com>
2024-09-08 19:47:29 +0545

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "countdown-to-flicks"
+  base: "/countdown-to-flicks/"
 })
